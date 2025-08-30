a68fd6d9ffea49667043f0ffb550ff4083b621b8
Rick <rick.kohjiaxuan@gmail.com>
2025-08-30 13:18:47 +0800

    Print proper chart messages in main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,5 @@
 import { ChartPieLabel } from "@/components/custom/pie-chart";
 import { getFeatureCount, getFeatures } from "@/services/featureService";
-import FeaturesTable from "@/components/custom/features-table";
 import { transformFeaturesToChart } from "@/helpers/transformToChart";
 import { NumberCard } from "@/components/custom/number-card";
 import { getSourceCount } from "@/services/sourceService";
@@ -14,6 +13,20 @@ export default async function Home() {
   const { data, dataKey, nameKey, chartConfig } =
     transformFeaturesToChart(features);
 
+  // Find the status with the highest count from the features array
+  const statusCounts = features.reduce<Record<string, number>>(
+    (acc, feature) => {
+      acc[feature.status] = (acc[feature.status] || 0) + 1;
+      return acc;
+    },
+    {}
+  );
+  const status_with_highest_count = Object.entries(statusCounts).reduce(
+    (maxStatus, [status, count]) =>
+      count > maxStatus.count ? { status, count } : maxStatus,
+    { status: "", count: 0 }
+  ).status;
+
   return (
     <main className="flex flex-row gap-4 p-4">
       <div className="flex flex-col gap-4 flex-1">
@@ -23,20 +36,31 @@ export default async function Home() {
             description="Total number of sources"
             value={source_count}
             footerText="Total sources"
-            subFooterText="Total sources"
+            // subFooterText="Total sources"
           />
           <NumberCard
             title="Total Features"
             description="Total number of features"
             value={feature_count}
             footerText="Total features"
-            subFooterText="Total features"
+            // subFooterText="Total features"
           />
           <ChartPieLabel
             title="Status Distribution"
             description="Features by status"
-            header="Trending up by 5.2% this month"
-            subheader="Showing total visitors for the last 6 months"
+            header={
+              `Majority are ${status_with_highest_count}. ` +
+              (status_with_highest_count === "pass"
+                ? "You're good to go!"
+                : status_with_highest_count === "warning"
+                ? "Some need attention."
+                : status_with_highest_count === "pending"
+                ? "Still working on it."
+                : status_with_highest_count === "critical"
+                ? "Requires your urgent attention!"
+                : "")
+            }
+            subheader="Showing total composition of feature statuses"
             dataKey={dataKey}
             nameKey={nameKey}
             chartData={data}
